3831711bf18b3b66dbafa4c88634b9e33923fb45
Cesar-CCC <cusijustoc@gmail.com>
2022-11-07 22:30:14 -0500

    estilos_concurso3

--- resources/views/calificacion/index.blade.php
+++ resources/views/calificacion/index.blade.php
@@ -3,11 +3,38 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Roles</h1>
+<div class="text-center">
+    <h1>Calificar</h1>
+</div>
 @stop
 
 @section('content')
-    <h1>holaa</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-3 cssproblemas overflow-auto border-right border-dark">
+            <button class="btn btn-primary w-100 mb-3">Ejercicio número 2</button>
+            <button class="btn btn-primary w-100 mb-3">Ejercicio número 2</button>
+        </div>
+        <div class="col cssproblemas overflow-auto">
+            <div class="row mt-5">
+                <div class="col d-flex justify-content-center">
+                    <a href="" class="btn btn-success w-50">Ver ejercicio</a>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <a href="" class="btn btn-success w-50">Ver solución</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://studio.code.org/projects/spritelab/K0sR3_Ah2W4nb504jAkYsp6Ju5qiJHOHGIJlZiZrXe4/embed" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- <h1>holaa</h1>
     <div class="embed-responsive embed-responsive-16by9">
         <iframe class="" allowfullscreen></iframe>
     </div>
@@ -16,13 +43,18 @@
     </div>
     <iframe src="" frameborder="0"></iframe>
     <iframe width="392" height="620" style="border: 0px;" src="https://studio.code.org/projects/applab/qDwDUrBeIUwrNe0m0TMIbT3HcyrwELwU83QKBA0OfjE/embed"></iframe>
-    <iframe width="392" height="620" style="border: 0px;" src="https://studio.code.org/projects/applab/7hAhxDtFRxADJFOGOMrRqMve63DAnVAx9vfptwOo8Un/embed?nosource"></iframe>ss
+    <iframe width="392" height="620" style="border: 0px;" src="https://studio.code.org/projects/applab/7hAhxDtFRxADJFOGOMrRqMve63DAnVAx9vfptwOo8Un/embed?nosource"></iframe>ss -->
 @stop
 @section('css')
-
+<style>
+    .cssproblemas {
+        height: 600px;
+    }
+</style>
 @stop
 @section('js')
-    <script> console.log('Hi!'); </script>
+<script>
+    console.log('Hi!');
+</script>
 
 @stop
-
